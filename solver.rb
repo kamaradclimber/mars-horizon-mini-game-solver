f2a2f20a99aaf84992d7ca6adaab7fb467740bfd
Grégoire Seux <kamaradclimber@gmail.com>
2020-05-02 20:01:01 +0200

    Optimize number of turns
    it will let more space for incidents
    
    Change-Id: I3ac1f60390f14899d7b7181dc573f557e3749725

--- solver.rb
+++ solver.rb
@@ -59,6 +59,12 @@ class State
       end
     end.sum
   end
+
+  def achieved?(objective)
+    objective.all? do |resource, quantity|
+      (@resources[resource] || 0) >= quantity
+    end
+  end
 end
 
 class World
@@ -73,7 +79,7 @@ class World
   # return [] if we already are at the solution
   # return [<Transformation>] if it leads to the solution
   def solve
-    return [] if @current_state.distance_of(@objective).zero?
+    return [] if @current_state.achieved?(@objective)
     return nil if @max_round <= 0
 
     possible_transformations = @transformations.select { |transformation| @current_state.can_apply?(transformation) }
@@ -93,13 +99,38 @@ class World
   end
 end
 
+class OptimalSolver
+  def initialize(transformations, objective, max_rounds, init_state)
+    @transformations = transformations
+    @objective = objective
+    @max_rounds = max_rounds
+    @init_state = init_state
+  end
+
+  def run
+    best_sol = nil
+    new_best_sol = Object.new
+    rounds = @max_rounds
+    until rounds.zero? || new_best_sol.nil?
+      print "Trying to solve in #{rounds} rounds..."
+      best_sol = new_best_sol
+      w = World.new(@transformations, @objective, rounds, @init_state)
+      new_best_sol = w.solve
+      rounds -= 1
+      puts (new_best_sol ? 'OK' : 'NOK')
+    end
+    best_sol
+  end
+end
+
+# Dummy test1
 state = State.new(coms: 0, electricity: 2)
 transformations = [
   Transformation.new(inputs: {}, outputs: { electricity: 1 }),
   Transformation.new(inputs: { electricity: 1 }, outputs: { data: 1 }),
   Transformation.new(inputs: { electricity: 1 }, outputs: { coms: 1 }),
 ]
-objective = { data: 2, coms: 1}
+objective = { data: 2, coms: 1 }
 
 w = World.new(transformations, objective, 4, state)
 solution = w.solve
@@ -108,3 +139,33 @@ raise 'Could not find any solution' unless solution
 solution.each do |t|
   puts t
 end
+
+# Mars flyby - part 2
+puts '-----'
+state = State.new(electricity: 5)
+transformations = [
+  Transformation.new(inputs: {}, outputs: { electricity: 1 }),
+
+  Transformation.new(inputs: { electricity: 2 }, outputs: { coms: 2 }),
+  Transformation.new(inputs: { data: 1 }, outputs: { coms: 2, nav: 1 }),
+  Transformation.new(inputs: { nav: 1 }, outputs: { coms: 2, data: 1 }),
+
+  Transformation.new(inputs: { electricity: 1 }, outputs: { data: 2 }),
+  Transformation.new(inputs: { electricity: 1, coms: 2 }, outputs: { data: 3 }),
+  Transformation.new(inputs: { nav: 2 }, outputs: { data: 3 }),
+
+  Transformation.new(inputs: { electricity: 1 }, outputs: { nav: 2 }),
+  Transformation.new(inputs: { data: 1 }, outputs: { coms: 1, nav: 2 }),
+  Transformation.new(inputs: { coms: 1, electricity: 1 }, outputs: { nav: 4 }),
+]
+objective = { coms: 5, data: 8, nav: 3 }
+
+max_rounds = 12
+solver = OptimalSolver.new(transformations, objective, max_rounds, state)
+solution = solver.run
+raise 'Could not find any solution' unless solution
+
+solution.each_with_index do |t, i|
+  puts "#{i + 1}: #{t}"
+end
+puts "Using #{solution.size} rounds out of #{max_rounds}"
