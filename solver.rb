3439f4bdeda2fa46e8d5501022c0b6a80527634a
Grégoire Seux <kamaradclimber@gmail.com>
2020-05-03 10:39:24 +0200

    Support heat/crew special cases
    Change-Id: I94ae79892f8fc719ac006ae94914123d61dad46d

--- solver.rb
+++ solver.rb
@@ -64,12 +64,15 @@ class State
 end
 
 class World
-  def initialize(transformations, objective, remaining_rounds, init_state, loose_1thrust_every_3rounds: false)
+  def initialize(transformations, objective, remaining_rounds, init_state,
+                 loose_1thrust_every_3rounds: false, max_heat: nil, with_crew: false)
     @transformations = transformations
     @objective = objective
     @remaining_rounds = remaining_rounds
     @current_state = init_state
     @loose_1thrust_every_3rounds = loose_1thrust_every_3rounds
+    @max_heat = max_heat
+    @with_crew = with_crew
   end
 
   # return nil if nothing is possible
@@ -90,11 +93,21 @@ class World
     sorted_transformations.lazy.map do |transformation|
       future_state = @current_state.dup.apply(transformation)
       if @loose_1thrust_every_3rounds && ((@remaining_rounds - 1) % 3).zero?
-        puts 'Should decrease thrust'
         r = future_state.resources
         r[:thrust] -= 1 if r[:thrust] && r[:thrust] > 0
       end
-      world = World.new(@transformations, @objective, @remaining_rounds - 1, future_state, loose_1thrust_every_3rounds: @loose_1thrust_every_3rounds)
+      if @max_heat && ((@remaining_rounds - 1) % 3).zero?
+        r = future_state.resources
+        r[:heat] ||= 0
+        r[:heat] += 2
+        return nil if r[:heat] >= 5 # heat failure
+      end
+      if @with_crew && ((@remaining_rounds - 1) % 4).zero? # FIXME: every 4 round is hard coded
+        r = future_state.resources
+        r[:crew] = @with_crew # reset crew
+      end
+      world = World.new(@transformations, @objective, @remaining_rounds - 1, future_state,
+                        loose_1thrust_every_3rounds: @loose_1thrust_every_3rounds, max_heat: @max_heat, with_crew: @with_crew)
       res = world.solve
       [transformation] + res if res
     end.select(&:itself).first
@@ -102,12 +115,15 @@ class World
 end
 
 class OptimalSolver
-  def initialize(transformations, objective, max_rounds, init_state, loose_1thrust_every_3rounds: false)
+  def initialize(transformations, objective, max_rounds, init_state,
+                 loose_1thrust_every_3rounds: false, max_heat: nil, with_crew: false)
     @transformations = transformations
     @objective = objective
     @max_rounds = max_rounds
     @init_state = init_state
     @loose_1thrust_every_3rounds = loose_1thrust_every_3rounds
+    @max_heat = max_heat
+    @with_crew = with_crew
   end
 
   def run
@@ -117,7 +133,7 @@ class OptimalSolver
     until rounds.zero? || new_best_sol.nil?
       print "Trying to solve in #{rounds} rounds..."
       best_sol = new_best_sol
-      w = World.new(@transformations, @objective, rounds, @init_state, loose_1thrust_every_3rounds: @loose_1thrust_every_3rounds)
+      w = World.new(@transformations, @objective, rounds, @init_state, loose_1thrust_every_3rounds: @loose_1thrust_every_3rounds, max_heat: @max_heat, with_crew: @with_crew)
       new_best_sol = begin
                        Timeout.timeout(15) { w.solve }
                      rescue Timeout::Error
