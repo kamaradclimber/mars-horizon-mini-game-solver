16ed04ec5ad6695d46f6dab21f5449dc042fcbb3
Grégoire Seux <kamaradclimber@gmail.com>
2020-05-03 14:00:37 +0200

    Support correctly variable number of rounds per turn
    Change-Id: I3e3ee8aa998f509275744230d1aadfe388ffebc3

--- solver.rb
+++ solver.rb
@@ -64,17 +64,17 @@ class State
 end
 
 class World
-  def initialize(transformations, objective, remaining_rounds, init_state,
-                 loose_1thrust_every_3rounds: false, max_heat: nil, with_crew: false,
-                 heat_incr: 2)
+  def initialize(transformations, objective, remaining_rounds, init_state, opts = {})
     @transformations = transformations
     @objective = objective
     @remaining_rounds = remaining_rounds
     @current_state = init_state
-    @loose_1thrust_every_3rounds = loose_1thrust_every_3rounds
-    @max_heat = max_heat
-    @with_crew = with_crew
-    @heat_incr = heat_incr
+    @opts = opts
+    @loose_1thrust_every_3rounds = opts[:loose_1thrust_every_3rounds] || false
+    @max_heat = opts[:max_heat]
+    @with_crew = opts[:with_crew] || false
+    @heat_incr = opts[:heat_incr] || 2
+    @rounds_per_turn = opts[:rounds_per_turn] || 3
   end
 
   # return nil if nothing is possible
@@ -94,23 +94,21 @@ class World
 
     sorted_transformations.lazy.map do |transformation|
       future_state = @current_state.dup.apply(transformation)
-      if @loose_1thrust_every_3rounds && ((@remaining_rounds - 1) % 3).zero?
+      if @loose_1thrust_every_3rounds && ((@remaining_rounds - 1) % @rounds_per_turn).zero?
         r = future_state.resources
         r[:thrust] -= 1 if r[:thrust] && r[:thrust] > 0
       end
-      if @max_heat && ((@remaining_rounds - 1) % 3).zero?
+      if @max_heat && ((@remaining_rounds - 1) % @rounds_per_turn).zero?
         r = future_state.resources
         r[:heat] ||= 0
         r[:heat] += @heat_incr
         return nil if r[:heat] >= 5 # heat failure
       end
-      if @with_crew && ((@remaining_rounds - 1) % 4).zero? # FIXME: every 4 round is hard coded
+      if @with_crew && ((@remaining_rounds - 1) % @rounds_per_turn).zero?
         r = future_state.resources
         r[:crew] = @with_crew # reset crew
       end
-      world = World.new(@transformations, @objective, @remaining_rounds - 1, future_state,
-                        loose_1thrust_every_3rounds: @loose_1thrust_every_3rounds, max_heat: @max_heat, with_crew: @with_crew,
-                        heat_incr: @heat_incr)
+      world = World.new(@transformations, @objective, @remaining_rounds - 1, future_state, @opts)
       res = world.solve
       [transformation] + res if res
     end.select(&:itself).first
@@ -118,17 +116,12 @@ class World
 end
 
 class OptimalSolver
-  def initialize(transformations, objective, max_rounds, init_state,
-                 loose_1thrust_every_3rounds: false, max_heat: nil, with_crew: false,
-                 heat_incr: 2)
+  def initialize(transformations, objective, max_rounds, init_state, opts = {})
     @transformations = transformations
     @objective = objective
     @max_rounds = max_rounds
     @init_state = init_state
-    @loose_1thrust_every_3rounds = loose_1thrust_every_3rounds
-    @max_heat = max_heat
-    @with_crew = with_crew
-    @heat_incr = heat_incr
+    @opts = opts
   end
 
   def run
@@ -138,7 +131,7 @@ class OptimalSolver
     until rounds.zero? || new_best_sol.nil?
       print "Trying to solve in #{rounds} rounds..."
       best_sol = new_best_sol
-      w = World.new(@transformations, @objective, rounds, @init_state, loose_1thrust_every_3rounds: @loose_1thrust_every_3rounds, max_heat: @max_heat, with_crew: @with_crew, heat_incr: @heat_incr)
+      w = World.new(@transformations, @objective, rounds, @init_state, @opts)
       new_best_sol = begin
                        Timeout.timeout(15) { w.solve }
                      rescue Timeout::Error
@@ -146,13 +139,6 @@ class OptimalSolver
                        nil
                      end
       puts(new_best_sol ? 'OK' : 'NOK')
-      # if new_best_sol
-      #  puts "  Known solution in #{rounds} rounds"
-      #  new_best_sol.each do |transition|
-      #    print '  '
-      #    puts transition
-      #  end
-      # end
       rounds -= 1
     end
     best_sol
